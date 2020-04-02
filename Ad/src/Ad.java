f96939a8e20f9a93ff8743b5cfd4f5df610a5b0d
Arijit Dey <arijitrollno28@gmail.com>
2020-04-02 12:31:08 +0530

    Ad change

--- Ad/src/Ad.java
+++ Ad/src/Ad.java
@@ -3,7 +3,8 @@ public class Ad {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.print("hi");
+System.out.println("hi.......I am Arijit");
+System.out.println("hi.......I am Arijit2");
 	}
 
 }
